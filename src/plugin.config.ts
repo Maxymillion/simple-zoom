c9da2dfe14a6c59b960dd21fcc44185240cc8646
Max van Essen <max.vessen@icloud.com>
2023-02-16 10:01:03 +0100

    Update Plugin Name

--- src/plugin.config.ts
+++ src/plugin.config.ts
@@ -1,4 +1,4 @@
 export const pluginConfig = {
-	name: "Simple font-size controls",
-	description: "An Obsidian plugin which adds control over the fontsize of the (Markdown/Live/Preview) Editor",
+	name: "Simple Zoom",
+	description: "An Obsidian plugin which adds more zoom control",
 }
